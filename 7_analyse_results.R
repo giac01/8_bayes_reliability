af01f7220a74a53841aefc77ea5eb8589d0e2842
giac01 <bignardig@outlook.com>
2024-04-23 16:00:42 +0100

    updated reinforcement learning code

--- 7_analyse_results.R
+++ 7_analyse_results.R
@@ -14,65 +14,65 @@ results_files = list.files(results_path,
                            recursive = FALSE,
                            full.names = TRUE
 )
-results = lapply(results_files, function(x) readRDS(x))
+
+results = lapply(results_files[!grepl("pop",results_files)], function(x) readRDS(x))
 
 results = do.call("c", results)
 
-# results_pop = readRDS(file = file.path("results","7_results_pop_c.rds"))
+results_pop = readRDS(file = results_files[grepl("pop",results_files)])
 
 # Sample size used in "population" simulations. 
 # results_pop[[1]]$settings$n_pps
 
-## Extract our estimates of the "true" reliabilities ---------------------------
+# Extract Population Reliability simulations -----------------------------------
 
-# simulated_reliabilities         = sapply(results_pop, function(x) x$cor_bayes_estimate_true$estimate^2) 
-# simulated_reliabilities         = ifelse(is.na(simulated_reliabilities), 0,  simulated_reliabilities)
-# sens_mean                       = sapply(results_pop, function(x) x$settings$sens_mean)
-# sens_sigma                      = sapply(results_pop, function(x) x$settings$sens_sigma) 
-# k_mean                          = sapply(results_pop, function(x) x$settings$k_mean) 
-# k_sigma                         = sapply(results_pop, function(x) x$settings$k_sigma) 
-# n_items                         = sapply(results_pop, function(x) x$settings$n_items) 
-# n_pps                           = sapply(results_pop, function(x) x$settings$n_pps) 
-# 
-# settings_used = paste(sens_mean, sens_sigma, k_mean, k_sigma, n_items, sep ="_")
-# 
-# table( table(unique(settings_used))) # should equal 1
-# 
-# results_table = params_list
-# 
-# results_table$settings_used = paste(results_table$sens_mean, 
-#                                     results_table$sens_sigma, 
-#                                     results_table$k_mean, 
-#                                     results_table$k_sigma, 
-#                                     results_table$n_items, 
-#                                     sep = "_")
+results_pop[[1]]$settings$n_pps
 
-## Create results table for simulation results ---------------------------------
+simulated_reliabilities     = sapply(results_pop, function(x) x$cor_bayes_estimate_true$estimate^2)
+simulated_reliabilities     = ifelse(is.na(simulated_reliabilities), 0,  simulated_reliabilities)
+sample_sizes                = sapply(results_pop, function(x) x$settings$n_pps)
+n_trials                    = sapply(results_pop, function(x) x$settings$n_trials)
+learning_rate_mean          = sapply(results_pop, function(x) x$settings$learning_rate_mean)
+learning_rate_sd            = sapply(results_pop, function(x) x$settings$learning_rate_sd)
+decision_noise_mean         = sapply(results_pop, function(x) x$settings$decision_noise_mean)
+decision_noise_sd           = sapply(results_pop, function(x) x$settings$decision_noise_sd)
+# prob_real                   = sapply(results_pop, function(x) x$settings$prob_real[1])
+
+settings_used = paste(n_trials,learning_rate_mean, learning_rate_sd, decision_noise_mean,decision_noise_sd, sep = "_")
+
+# Create results table----------------------------------------------------------
+did_model_run = rep(NA, length(results))
+for(i in 1:length(results)){
+  did_model_run[i] = !identical(results[[i]]$cor_bayes_estimate_true,9999)
+}
+
+results = results[(did_model_run)]
+
 results_table = data.frame(i = 1:length(results))
 
-results[[14]]$cor_bayes_estimate_true$conf.int[2]
 
 # Pop Reliability estimates
-# results_table$pop_rel         = simulated_reliabilities[match(results_table$settings_used,settings_used)]
-# results_table$true_score_cor2 = sapply(results, function(x) x$cor_bayes_estimate_true$estimate^2) %>% as.numeric()
-# results_table$true_score_cor2 = ifelse(is.na(results_table$true_score_cor2), 0 , results_table$true_score_cor2)
+# results_table$true_score_cor2         = simulated_reliabilities[match(results_table$settings_used,settings_used)]
+results_table$true_score_cor2 = sapply(results, function(x) x$cor_bayes_estimate_true$estimate^2) %>% as.numeric()
+results_table$true_score_cor2 = ifelse(is.na(results_table$true_score_cor2), 0 , results_table$true_score_cor2)
+results_table$avg_true_score_coverage = sapply(results, function(x) x$avg_true_score_coverage) %>% as.numeric()
 
 # Settings
 
-results_table$n_pps           = sapply(results, function(x) x$settings$n_pps) %>% as.numeric()
-results_table$sample_sizes           = sapply(results, function(x) x$settings$n_pps) %>% as.numeric()
-results_table$n_trials           = sapply(results, function(x) x$settings$n_trials) %>% as.numeric()
-results_table$learning_rate_mean           = sapply(results, function(x) x$settings$learning_rate_mean) %>% as.numeric()
-results_table$learning_rate_sd           = sapply(results, function(x) x$settings$learning_rate_sd) %>% as.numeric()
-results_table$decision_noise_mean           = sapply(results, function(x) x$settings$decision_noise_mean) %>% as.numeric()
-results_table$decision_noise_sd           = sapply(results, function(x) x$settings$decision_noise_sd) %>% as.numeric()
+results_table$n_pps               = sapply(results, function(x) x$settings$n_pps) %>% as.numeric()
+results_table$sample_sizes        = sapply(results, function(x) x$settings$n_pps) %>% as.numeric()
+results_table$n_trials            = sapply(results, function(x) x$settings$n_trials) %>% as.numeric()
+results_table$learning_rate_mean  = sapply(results, function(x) x$settings$learning_rate_mean) %>% as.numeric()
+results_table$learning_rate_sd    = sapply(results, function(x) x$settings$learning_rate_sd) %>% as.numeric()
+results_table$decision_noise_mean = sapply(results, function(x) x$settings$decision_noise_mean) %>% as.numeric()
+results_table$decision_noise_sd   = sapply(results, function(x) x$settings$decision_noise_sd) %>% as.numeric()
 
 # Relative Measurement Precision
-results_table$rmp_est         = sapply(results, function(x) x$rmp[1]) %>% as.numeric()
-results_table$rmp_lb          = sapply(results, function(x) x$rmp[2]) %>% as.numeric()
-results_table$rmp_ub          = sapply(results, function(x) x$rmp[3]) %>% as.numeric()
-results_table$rmp_ci_length   = results_table$rmp_ub - results_table$rmp_lb
-results_table$rmp_pd          = sapply(results, function(x) x$rmp_pd) %>% as.numeric()
+results_table$rmp_est       = sapply(results, function(x) x$rmp[1]) %>% as.numeric()
+results_table$rmp_lb        = sapply(results, function(x) x$rmp[2]) %>% as.numeric()
+results_table$rmp_ub        = sapply(results, function(x) x$rmp[3]) %>% as.numeric()
+results_table$rmp_ci_length = results_table$rmp_ub - results_table$rmp_lb
+results_table$rmp_pd        = sapply(results, function(x) x$rmp_pd) %>% as.numeric()
 
 # Other metrics
 
@@ -83,51 +83,62 @@ results_table$cor_bayes_estimate_true_lb[is.na(results_table$cor_bayes_estimate_
 results_table$cor_bayes_estimate_true_ub = sapply(results, function(x) x$cor_bayes_estimate_true$conf.int[2]) %>% as.numeric()
 results_table$cor_bayes_estimate_true_ub[is.na(results_table$cor_bayes_estimate_true_ub)] = 0
 
-# Performance Measures ----------------------------------------------------------
+# Add population reliability
 
-# Empty - calculed on the fly below now
+results_table$settings_used = paste(
+  results_table$n_trials,
+  results_table$learning_rate_mean, 
+  results_table$learning_rate_sd, 
+  results_table$decision_noise_mean, 
+  results_table$decision_noise_sd, 
+  sep = "_"
+)
 
-# Reminder of different settings used ------------------------------------------
+results_table$settings_used_with_npps = paste(
+  results_table$n_pps,
+  results_table$n_trials,
+  results_table$learning_rate_mean, 
+  results_table$learning_rate_sd, 
+  results_table$decision_noise_mean, 
+  results_table$decision_noise_sd, 
+  sep = "_"
+)
 
-params_list %>%
-  filter(run_rep ==1)
+results_table$pop_rel = simulated_reliabilities[match(results_table$settings_used, settings_used)]
 
-# Results Table-----------------------------------------------------------------
+## calculate LOO cross-validated true_score_cor  -----------------------------------
+results_table$true_score_cor2_loo = NA
 
-results_table %>%
-  mutate(
-    sample_sizes = factor(sample_sizes),
-    sens_sigma  = factor(sens_sigma),
-    n_items     = factor(n_items)
-  ) %>%
-  ggplot(aes( x = true_score_cor2, y = rmp_est, shape = sample_sizes, col = n_items)) + 
-  geom_point() + 
-  geom_abline(intercept = 0, slope = 1) +
-  facet_wrap(~sens_sigma)
+for (i in 1:nrow(results_table)){
+  results_table$true_score_cor2_loo[i] = results_table[-i,] %>%
+    dplyr::filter(settings_used_with_npps==results_table[-i,"settings_used_with_npps"]) %>% 
+    pull(true_score_cor2) %>%
+    mean()
+}
 
-# results_table_long = results_table %>%
-#   rowid_to_column() %>%
-#   pivot_longer(cols = c(rmp_est,rmp_lb,rmp_ub, sh_est, sh_lb, sh_ub), names_to = c("name", ".value"), names_pattern = "(rmp|sh)_(.*)") 
 
 # Analysis of coverage/bias ---------------------------------------------------
 
 results_table %>%
   group_by(learning_rate_sd, n_trials, sample_sizes) %>%       # aggregating over sample-sizes & sens_mean
-  mutate(pop_rel     = mean(cor_bayes_estimate_true^2)) %>%
+  mutate(pop_rel = mean(true_score_cor2_loo)) %>% 
+  mutate(mean_rmp_est = mean(rmp_est)) %>% 
   mutate(
-    # pop_rel     = mean(cor_bayes_estimate_true^2),
     est        = rmp_est,
     ub         = rmp_ub,
     lb         = rmp_lb,
     difference = est - pop_rel,
     ci_correct = (lb <= pop_rel & ub >= pop_rel),
-    ci_length  = ub - lb
+    ci_length  = ub - lb,
+    ci_mean_correct = (lb <= true_score_cor2_loo & ub >= true_score_cor2_loo),
+    ci_bias_elimated_correct = (lb <= mean_rmp_est & ub >= mean_rmp_est),
+    mean_rmp_est = mean(rmp_est)
   ) %>%
   summarise(
     n = n(),
-    pop_rel_mean    = mean(pop_rel),
-    # pop_rel_sd      = sd(pop_rel),
-    # avg_cor2        = mean(true_score_cor2),
+    pop_rel     = mean(pop_rel),
+    pop_rel_sd      = sd(pop_rel),
+    avg_cor2        = mean(true_score_cor2),
     mean        = mean(est),
     bias        = mean(difference),
     bias_se     = sqrt(1/(n*(n-1))*sum((est-mean)^2)),
@@ -138,22 +149,22 @@ results_table %>%
     coverage_se = sqrt((coverage*(1-coverage))/n),
     coverage_lb = coverage - 1.96*coverage_se,
     coverage_ub = coverage + 1.96*coverage_se,
-    mean_ci_length = mean(ci_length)
-    # perc_diag_divergences_binary = sum(diag_divergences_binary)/n,
-    # perc_diag_ebfmi_binary       = sum(diag_ebfmi_binary)/n
+    coverage_true_score_cor = length(which(ci_mean_correct))/length(ci_mean_correct),
+    coverage_bias_eliminated = length(which(ci_bias_elimated_correct))/length(ci_bias_elimated_correct),
+    mean_ci_length = mean(ci_length),
+    mean_ts_coverage = mean(avg_true_score_coverage)
   )  %>%
-  ungroup()
-
+  ungroup() %>% 
+  # knitr::kable(digits =2)
   select(-pop_rel_sd, -coverage_se) %>%
-  select(pop_rel_mean, everything()) %>%
-  arrange(sens_sigma, n_items, k_sigma, sample_sizes,name) %>%
+  select(pop_rel,avg_cor2,sample_sizes,learning_rate_sd, everything()) %>%
   gt() %>%
   fmt(
     columns = where(is.numeric),
     fns = function(x) gbtoolbox::apa_num(x, n_decimal_places = 3)
   ) %>%
   fmt_number(
-    columns = c(n, n_items, perc_diag_divergences_binary, perc_diag_ebfmi_binary),
+    columns = c(n, n_trials),
     decimals = 0
   ) %>%
   fmt_percent(
@@ -164,22 +175,18 @@ results_table %>%
     sample_sizes ~ "{{n_obs}}",
     n            ~ "{{n_sim}}",      # avg_cor2     ~ "{{:rho:_:theta:,x^2}}",
     avg_cor2      ~ "{{mean[:rho:_:theta:,x^2 ]}}",
-    pop_rel_mean ~ "{{R_pop}}",
-    # sens_mean    ~ "{{:mu:_d'}}",
-    sens_sigma   ~ "{{:sigma:_di'}}",
-    k_sigma      ~ "{{:sigma:_:kappa:}}",
-    n_items      ~ "{{n_trials}}",
+    pop_rel ~ "{{R_pop}}",
+    n_trials      ~ "{{n_trials}}",
     bias           ~ "bias",
     bias_lb  ~ "LB",
     bias_ub  ~ "UB",
-    mean_ci_length ~ "Mean Length",
-    name         ~ "Estimator",
-    perc_diag_divergences_binary ~ "% Divergent Transitions",
-    perc_diag_ebfmi_binary ~  "% Low E-BFMI"
-    
+    coverage_lb  ~ "LB",
+    coverage_ub  ~ "UB",
+    mean_ci_length ~ "Mean Length"
   )  %>%
   tab_spanner(label = "Bias 95% CI", columns = c(bias, bias_lb, bias_ub)) %>%
-  tab_spanner(label = "Simulation Parameters", columns = c(pop_rel_mean, sens_sigma, n_items, k_sigma, n, sample_sizes,name)) %>%
+  tab_spanner(label = "Coverage 95% CI", columns = c(coverage, coverage_lb, coverage_ub)) %>%
+  tab_spanner(label = "Simulation Parameters", columns = c(pop_rel,avg_cor2,sample_sizes,learning_rate_sd)) %>%
   tab_spanner(label = "Estimator Performance", columns = c(mean, mae, MSE, contains("bias"))) %>%
   tab_spanner(label = "Credible Interval Performance", columns = c(starts_with("coverage"),"mean_ci_length")) %>%
   tab_footnote(
@@ -189,12 +196,86 @@ results_table %>%
     style = cell_fill(color = "lightgray"),
     locations = cells_body(
       columns = everything(),
-      rows = which((name == "rmp"))
+      rows = which((sample_sizes == 200))
     )
-  ) %>% 
-  gt::cols_hide(c(mae,avg_cor2, bias_se, mean))
-
-gtsave(filename = file.path("results","6_results_table_1.html"))
+  ) %>%
+  gt::cols_hide(c(mae, bias_se, mean))
 
 
+gtsave(filename = file.path("results","7_results_table_1.html"))
 
+# Examine coverage issues in more detail ---------------------------------------
+
+results_table %>%
+  mutate(
+    sample_sizes = factor(sample_sizes),
+    learning_rate_sd  = factor(learning_rate_sd),
+    n_trials     = factor(n_trials),
+    true_score_cor2 = cor_bayes_estimate_true^2
+  ) %>%
+  ggplot(aes( x = pop_rel, y = rmp_est, shape = sample_sizes, col = n_trials)) + 
+  geom_point() + 
+  geom_abline(intercept = 0, slope = 1) +
+  facet_wrap(~learning_rate_sd + sample_sizes)
+
+results_table %>%
+  mutate(
+    sample_sizes = factor(sample_sizes),
+    learning_rate_sd  = factor(learning_rate_sd),
+    n_trials     = factor(n_trials),
+    true_score_cor2 = cor_bayes_estimate_true^2
+  ) %>%
+  ggplot(aes( x = pop_rel, y = true_score_cor2, shape = sample_sizes, col = n_trials)) + 
+  geom_point() + 
+  geom_abline(intercept = 0, slope = 1) +
+  facet_wrap(~learning_rate_sd + sample_sizes)
+
+results_table %>%
+  # filter(sample_sizes==1000) %>%
+  # filter(n_trials ==200) %>%
+  # filter(learning_rate_sd==.15) %>%
+  mutate(
+    sample_sizes = factor(sample_sizes),
+    learning_rate_sd  = factor(learning_rate_sd),
+    n_trials     = factor(n_trials),
+    true_score_cor2 = cor_bayes_estimate_true^2
+  ) %>%
+  arrange(rmp_est) %>%
+  group_by(sample_sizes,learning_rate_sd, n_trials) %>%
+  mutate(i = 1:n()) %>%
+  ggplot(aes( x = i, y = rmp_est, shape = sample_sizes, col = sample_sizes)) + 
+  geom_point() + 
+  geom_errorbar(aes(ymin = rmp_lb, ymax = rmp_ub)) +
+  geom_hline(aes(yintercept =pop_rel)) +
+  coord_cartesian(ylim =c(0,1)) 
+
+
+results_table %>%
+  group_by(learning_rate_sd, n_trials, sample_sizes) %>%       # aggregating over sample-sizes & sens_mean
+  mutate(pop_rel = mean(true_score_cor2_loo)) %>% 
+  mutate(
+    sample_sizes = factor(sample_sizes),
+    learning_rate_sd  = factor(learning_rate_sd),
+    n_trials     = factor(n_trials),
+    true_score_cor2 = cor_bayes_estimate_true^2
+  ) %>%
+  arrange(rmp_est) %>%
+  # group_by(sample_sizes,learning_rate_sd, n_trials) %>%
+  mutate(i = 1:n()) %>%
+  ggplot(aes( x = i, y = rmp_est, shape = sample_sizes, col = sample_sizes)) + 
+  geom_point() + 
+  geom_errorbar(aes(ymin = rmp_lb, ymax = rmp_ub)) +
+  geom_hline(aes(yintercept =pop_rel)) +
+  coord_cartesian(ylim =c(0,1)) +
+  facet_wrap(~ learning_rate_sd+ n_trials+ sample_sizes)
+
+
+
+
+test = 
+results_table %>%
+  filter(sample_sizes==200) %>%
+  filter(n_trials ==50) %>%
+  filter(learning_rate_sd==0) 
+
+table((test$rmp_lb<=0) & (test$rmp_ub>=0))
